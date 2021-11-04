f5c6adac340136242bcbb064f11bdbe6a114fc53
Kazuki HASEGAWA <k@zuki.dev>
2021-11-04 15:58:08 +0900

    tidyup install.sh(indent, function_name...)

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ set -Ceuo pipefail
 _link_files() {
   local src_path="$1"
   local dest_path="$2"
-  find $src_path  -maxdepth 1 -type f -name '*' -o -name '.*' | xargs -I FILE ln -sf FILE $dest_path
+  find $src_path  -maxdepth 1 -type f -name '*' -o -name '.*' | xargs -I FILE ln -sf FILE "$dest_path"
 }
 
 _make_directory() {
   local dpath="$1"
-  if [ ! -d $dpath ]; then
-    mkdir -p $dpath
+  if [ ! -d "$dpath" ]; then
+    mkdir -p "$dpath"
   fi
 }
 
@@ -26,64 +26,64 @@ _make_directory() {
 main() {
   # make ~/.config
   local config_path=$HOME/.config
-  _make_directory $config_path
+  _make_directory "$config_path"
 
   # make ~/.cache
   local cache_path=$HOME/.cache
-  _make_directory $cache_path
+  _make_directory "$cache_path"
 
   # make ~/bin
   local bin_path=$HOME/bin
-  _make_directory $bin_path
-  _link_files $PWD/bin $bin_path
+  _make_directory "$bin_path"
+  _link_files "$PWD"/bin "$bin_path"
 
   # Put Neovim configurations
   local nvim_cfg_dir_path=$config_path/nvim
   local pwd_nvim_cfg_dir_path=$PWD/.config/nvim
-  _make_directory $nvim_cfg_dir_path
-  _make_directory $nvim_cfg_dir_path/ftplugin
-  _link_files $pwd_nvim_cfg_dir_path $nvim_cfg_dir_path/
-  _link_files $pwd_nvim_cfg_dir_path/ftplugin $nvim_cfg_dir_path/ftplugin
-  ln -sf $pwd_nvim_cfg_dir_path/filetype.vim $nvim_cfg_dir_path/
-  ln -sf $pwd_nvim_cfg_dir_path/dein.toml $nvim_cfg_dir_path/
-  ln -sf $pwd_nvim_cfg_dir_path/dein_lazy.toml $nvim_cfg_dir_path/
+  _make_directory "$nvim_cfg_dir_path"
+  _make_directory "$nvim_cfg_dir_path"/ftplugin
+  _link_files "$pwd_nvim_cfg_dir_path" "$nvim_cfg_dir_path"/
+  _link_files "$pwd_nvim_cfg_dir_path"/ftplugin "$nvim_cfg_dir_path"/ftplugin
+  ln -sf "$pwd_nvim_cfg_dir_path"/filetype.vim "$nvim_cfg_dir_path"/
+  ln -sf "$pwd_nvim_cfg_dir_path"/dein.toml "$nvim_cfg_dir_path"/
+  ln -sf "$pwd_nvim_cfg_dir_path"/dein_lazy.toml "$nvim_cfg_dir_path"/
 
   # Put Tmux configurations
   local tmux_cfg_dir_path=$config_path/tmux
-  ln -sf $PWD/.tmux.conf $HOME/
-  _make_directory $tmux_cfg_dir_path
-  _link_files $PWD/.config/tmux $tmux_cfg_dir_path/
+  ln -sf "$PWD"/.tmux.conf "$HOME"/
+  _make_directory "$tmux_cfg_dir_path"
+  _link_files "$PWD"/.config/tmux "$tmux_cfg_dir_path"/
 
   # Put Git conifugrations
   local git_cfg_dir_path=$config_path/git
-  ln -sf $PWD/.gitconfig $HOME/
-  _make_directory $git_cfg_dir_path
-  _link_files $PWD/.config/git $git_cfg_dir_path/
+  ln -sf "$PWD"/.gitconfig "$HOME"/
+  _make_directory "$git_cfg_dir_path"
+  _link_files "$PWD"/.config/git "$git_cfg_dir_path"/
   _make_directory "$git_cfg_dir_path/hooks"
-  _link_files $PWD/.config/git/hooks "$git_cfg_dir_path/hooks"
-  touch $git_cfg_dir_path/local
+  _link_files "$PWD"/.config/git/hooks "$git_cfg_dir_path/hooks"
+  touch "$git_cfg_dir_path"/local
 
   # Put zsh configuration
-  ln -sf $PWD/.zshenv $HOME/
+  ln -sf "$PWD"/.zshenv "$HOME"/
   local zdir=$config_path/zsh
-  _make_directory $zdir
-  _link_files $PWD/.config/zsh $zdir
-  touch $zdir/.zshrc.local
+  _make_directory "$zdir"
+  _link_files "$PWD"/.config/zsh "$zdir"
+  touch "$zdir"/.zshrc.local
 
   # direnv
-  ln -sf $PWD/.direnvrc $HOME/
+  ln -sf "$PWD"/.direnvrc "$HOME"/
 
   # Ruby
-  ln -sf $PWD/.gemrc $HOME/
-  ln -sf $PWD/.irbrc $HOME/
+  ln -sf "$PWD"/.gemrc "$HOME"/
+  ln -sf "$PWD"/.irbrc "$HOME"/
 
   ##
   # for macOS
   if os::is_darwin; then
     ##
     # Powerline Fonts
-    if [ "$(ls $HOME/Library/Fonts | grep -i powerline)" == "" ]; then
-      logger:info 'Installing powerline fonts...'
+    if [ "$(ls "$HOME"/Library/Fonts | grep -i powerline)" == "" ]; then
+      logger::info 'Installing powerline fonts...'
       git clone https://github.com/powerline/fonts.git
       cd fonts && ./install.sh && cd .. && rm -rf fonts
     fi
@@ -91,13 +91,13 @@ main() {
     ##
     # Homebrew
     local brew_dir_path=$HOME/.brew
-    if [ ! -d $brew_dir_path ]; then
-      logger:info 'Installing homebrew...'
-      mkdir $brew_dir_path \
+    if [ ! -d "$brew_dir_path" ]; then
+      logger::info 'Installing homebrew...'
+      mkdir "$brew_dir_path" \
         && curl -L https://github.com/Homebrew/brew/tarball/master \
-        | tar xz --strip 1 -C $brew_dir_path
+        | tar xz --strip 1 -C "$brew_dir_path"
     fi
-    $brew_dir_path/bin/brew bundle --no-lock --file ./brewfiles/darwin/Brewfile
+    "$brew_dir_path"/bin/brew bundle --no-lock --file ./brewfiles/darwin/Brewfile
   fi
 
   ##
@@ -113,7 +113,7 @@ main() {
     ##
     # Linuxbrew
     if [ ! -d /home/linuxbrew ]; then
-      logger:info "Installing linuxbrew..."
+      logger::info "Installing linuxbrew..."
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
     /home/linuxbrew/.linuxbrew/bin/brew bundle --no-lock --file ./brewfiles/ubuntu/Brewfile
@@ -121,12 +121,12 @@ main() {
 
   if os::is_wsl; then
     local win32yank_path=$HOME/bin/win32yank.exe
-    if [ ! -e $win32yank_path ]; then
+    if [ ! -e "$win32yank_path" ]; then
       echo "hoge"
       wget -O tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
       unzip -d tmp tmp/win32yank.zip
-      mv tmp/win32yank.exe $win32yank_path
-      chmod u+x $win32yank_path
+      mv tmp/win32yank.exe "$win32yank_path"
+      chmod u+x "$win32yank_path"
       rm -f tmp/win32yank.zip tmp/LICENSE tmp/README.md
     fi
   fi
