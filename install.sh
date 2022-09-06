f53e1c4aee23ab04ed73085d5d281d4925cc6dc8
K@zuki <k@zuki.dev>
2022-09-06 18:28:25 +0900

    Fix install.sh on Ubuntu

--- install.sh
+++ install.sh
@@ -6,6 +6,27 @@ source "$(dirname "$(dirname "$0")")/.config/zsh/.zshrc.functions"
 
 set -Ceuo pipefail
 
+if os::is_ubuntu; then
+  builder::execute sudo apt-get update
+  builder::package ca-certificates
+  builder::package build-essential
+  builder::package locales-all
+  builder::package git
+  builder::package curl
+  builder::package wget
+
+  if os::is_wsl; then
+    local win32yank_path=$HOME/bin/win32yank.exe
+    if [ ! -e "$win32yank_path" ]; then
+      wget -O tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+      unzip -d tmp tmp/win32yank.zip
+      mv tmp/win32yank.exe "$win32yank_path"
+      chmod u+x "$win32yank_path"
+      rm -f tmp/win32yank.zip tmp/LICENSE tmp/README.md
+    fi
+  fi
+fi
+
 builder::directory $DOT_CONFIG_PATH
 builder::directory $DOT_CACHE_PATH
 builder::directory $HOME/bin
@@ -28,39 +49,18 @@ builder::link $PWD/.direnvrc $HOME/.direnvrc
 builder::link $PWD/.gemrc $HOME/.gemrc
 builder::link $PWD/.irbrc $HOME/.irbrc
 
+# Homebrew or Linuxbrew
+local BREW_PATH
 if os::is_darwin; then
-  # Homebrew
-  local brew_dir_path=$HOME/.brew
-  if [ ! -d "$brew_dir_path" ]; then
-    builder::directory $brew_dir_path
-    builder::execute curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$brew_dir_path"
+  BREW_PATH=$HOME/.brew
+  if [ ! -d $BREW_PATH ]; then
+    builder::directory $BREW_PATH
+    builder::execute curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$BREW_PATH"
   fi
-  builder::execute "$brew_dir_path/bin/brew bundle --file=$PWD/Brewfile"
-fi
-
-# TODO: 
-if os::is_ubuntu; then
-  builder::execute apt-get update
-  builder::package ca-certificates
-  builder::package build-essential
-  builder::package locales-all
-
-  ##
-  # Linuxbrew
-  if [ ! -d /home/linuxbrew ]; then
-    logger::info "Installing linuxbrew..."
+elif os::is_ubuntu; then
+  BREW_PATH=/home/linuxbrew/.linuxbrew
+  if [ ! -d $BREW_PATH ]; then
     builder::execute /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
-  builder::execute /home/linuxbrew/.linuxbrew/bin/brew bundle --no-lock --file ./brewfiles/ubuntu/Brewfile
 fi
-
-if os::is_wsl; then
-  local win32yank_path=$HOME/bin/win32yank.exe
-  if [ ! -e "$win32yank_path" ]; then
-    wget -O tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
-    unzip -d tmp tmp/win32yank.zip
-    mv tmp/win32yank.exe "$win32yank_path"
-    chmod u+x "$win32yank_path"
-    rm -f tmp/win32yank.zip tmp/LICENSE tmp/README.md
-  fi
-fi
+builder::execute "$BREW_PATH/bin/brew bundle --file=$PWD/Brewfile"
